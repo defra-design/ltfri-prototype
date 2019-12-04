d58a5b48bc2e3194edde24ca3d03e21970cad364
Simon Hurst <simon@simonhurst.co.uk>
2019-12-04 14:00:19 +0000

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Long term flood risk information',
+  serviceName: 'Simon is the best',
 
   // Default port that prototype runs on
   port: '3000',
